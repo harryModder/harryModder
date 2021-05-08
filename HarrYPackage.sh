1b2af3b50ee8ccb4568043af32b1ea5019f67a18
harryModder <80776089+harryModder@users.noreply.github.com>
2021-05-08 12:37:05 +0530

    Update HarrYPackage.sh

--- HarrYPackage.sh
+++ HarrYPackage.sh
@@ -1,2 +1,1 @@
-Pkg install python && Pkg install python2 && pkg install php && pkg install pip
-&& Pkg install pip2 && Pkg install git && Pkg install wget && pkg install git -y && pkg install python -y pkg install python2 -y 
+Pkg install python
